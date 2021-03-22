c3c40102d2f11709df154ca59639fd23af045075
Michael Glössl <gloess190117@sr.htlweiz.at>
2021-03-22 13:06:26 +0100

    getset_done

---
+++ UIR_Rechner/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Gtk;
+
+namespace UIR_Rechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            calculator calc = new calculator();
+
+            calc.getSetR = 6.0;
+
+            Console.WriteLine(calc.getSetR);
+        }
+    }
+}
---
+++ UIR_Rechner/calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UIR_Rechner
+{
+    class calculator
+    {
+        private double _R = 0.0;
+        private double _U = 0.0;
+        private double _I = 0.0;
+
+        public double getSetR
+        {
+            get => _R;
+            set
+            {
+                _R = value;
+            }
+        }
+
+        public double getSetU
+        {
+            get => _U;
+            set
+            {
+                _U = value;
+            }
+        }
+
+        public double getSetI
+        {
+            get => _I;
+            set
+            {
+                _I = value;
+            }
+        }
+
+
+    }
+}
